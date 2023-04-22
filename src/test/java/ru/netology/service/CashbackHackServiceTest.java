b9090d4ef3e2af9922b1367b279ee1b788727b16
Ilya Bobrov <i.bobrov87@gmail.com>
2023-04-22 16:54:03 +0300

    JUnit4 used

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldLeaveTheRemainder1() {
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldLeaveTheRemainder2() {
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldLeaveTheRemainder3() {
+        int actual = service.remain(500);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldLeaveTheRemainder4() {
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldLeaveTheRemainder5() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
